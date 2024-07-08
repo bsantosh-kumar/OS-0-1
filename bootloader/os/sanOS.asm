c8644bf6cf41ff3dc2114293e3d4f0eee2a9a113
san <santosh.kernel.dev@gmail.com>
2024-07-09 01:50:43 +0530

    added dir. and makefile structure in bootloader directory

---
+++ bootloader/os/sanOS.asm
@@ -0,0 +1,4 @@
+;This file is a placeholder for the sanOS, that is going to be loaded 
+;from the bootloader
+mov eax, 0x55
+add eax, 0x01
--- bootloader/sanOS.asm
+++
@@ -1,4 +0,0 @@
-;This file is a placeholder for the sanOS, that is going to be loaded 
-;from the bootloader
-mov eax, 0x55
-add eax, 0x01
